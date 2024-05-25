fc6d0a313d0bafa50957b4bb6d1472dead1622dc
doan02012004 <doana1k24@gmail.com>
2024-05-25 10:03:28 +0700

    delete all file

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { FormAddComponent } from './form-add/form-add.component';
-import { AboutComponent } from './about/about.component';
-
-export const routes: Routes = [
-  {path:"about", component:AboutComponent}
-];
--- src/app/interfaces/Product.ts
+++
@@ -1,8 +0,0 @@
-
-export interface Iproduct {
-  id?: number,
-  name: string,
-  image: string,
-  price: number,
-  description: string
-}
